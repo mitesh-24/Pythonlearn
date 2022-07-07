6f19fa464823079b91436ff11f461b8ad13011eb
Mitesh kulkarni <miteshhk05@gmail.com>
2022-07-07 23:36:52 +0530

    Add files via upload
    A Rock, Paper, Scissors Game using Python

---
+++ Day4/RPS.py
@@ -0,0 +1,53 @@
+import random
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+set = [rock,paper,scissors]
+
+print("It is 1 for rock, 2 for paper, and 3 for scissors")
+user_choice = int(input("what is your choice?\n"))
+if user_choice > 3 or user_choice < 1:
+    print("Invalid Input")
+    exit("Byee")
+print(set [user_choice-1])
+
+computer_choice = random.randint(1,3)
+print("Computer chose")
+print (computer_choice)
+print(set [computer_choice-1])
+
+
+if computer_choice == 1 and user_choice == 2:
+    print("You win!")
+elif user_choice == 1 and computer_choice == 3:
+    print("You win!")
+elif user_choice == 3 and computer_choice == 2:
+    print("Youu win!")
+elif user_choice == computer_choice:
+    print("Its a draw")
+else:
+    print("You loose!")
