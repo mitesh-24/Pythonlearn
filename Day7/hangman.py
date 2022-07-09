17c4a592007e7ee4cb5ef5735a506c206e65e7fd
Mitesh kulkarni <miteshhk05@gmail.com>
2022-07-09 23:04:56 +0530

    Update hangman.py

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -3,10 +3,20 @@ words = ["mouse","elephant","pig"]
 
 chosen_word = random.choice(words)
 
+#for testing
+print(f"the chosen word is {chosen_word}")
+
+display = []
+
+for _ in range(len(chosen_word)):
+    display += ("_")
+print(display)
+
 guess = input("Enter your guess : ").lower()
 
-for letter in chosen_word:
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+    
+print(display)
