e6db4e10cf2f029a892d7305e74c5e96a2bce26b
Mitesh kulkarni <miteshhk05@gmail.com>
2022-07-10 17:55:01 +0530

    Update hangman.py

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,22 +1,91 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+end_of_game = False
 words = ["mouse","elephant","pig"]
-
 chosen_word = random.choice(words)
+word_length = (len(chosen_word))
 
+lives = 6
 #for testing
-print(f"the chosen word is {chosen_word}")
+print(f"pssst the chosen word is {chosen_word}")
 
 display = []
 
-for _ in range(len(chosen_word)):
+for _ in range(word_length):
     display += ("_")
-print(display)
 
-guess = input("Enter your guess : ").lower()
+while not end_of_game:
+    guess = input("Enter your guess : ").lower()
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-    
-print(display)
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("You lose!!")
+        
+    print(f"{' '.join(display)}")
+    if "_" not in display:
+        end_of_game = True
+        print("You won!")
+        
+    print(stages[lives])
