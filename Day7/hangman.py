f9b5671752f43786ad966ade86c2122eda85ee49
Mitesh kulkarni <miteshhk05@gmail.com>
2022-07-10 21:40:17 +0530

    Update hangman.py

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,91 +1,39 @@
 import random
+from hangart import stages, logo
+from hangwords import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-end_of_game = False
-words = ["mouse","elephant","pig"]
-chosen_word = random.choice(words)
-word_length = (len(chosen_word))
+print(logo)
+game_is_finished = False
+lives = len(stages) - 1
 
-lives = 6
-#for testing
-print(f"pssst the chosen word is {chosen_word}")
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 display = []
-
 for _ in range(word_length):
-    display += ("_")
+    display += "_"
 
-while not end_of_game:
-    guess = input("Enter your guess : ").lower()
+while not game_is_finished:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+    print(f"{' '.join(display)}")
 
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
-            print("You lose!!")
-        
-    print(f"{' '.join(display)}")
-    if "_" not in display:
-        end_of_game = True
-        print("You won!")
-        
+            game_is_finished = True
+            print("You lose.")
+    
+    if not "_" in display:
+        game_is_finished = True
+        print("You win.")
+
     print(stages[lives])
